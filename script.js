6cef32e1a4344ef3f56efa42bd6ed39e6464ba03
Caroline.Spaniol <carolinspaniol@gmail.com>
2023-12-05 15:21:27 +0100

    05.12.2023 Exercise 03

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 export function exercise01(args) {
   const input = args
   const result = []
-  
+
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
@@ -19,4 +19,19 @@ export function exercise01(args) {
 
 export function exercise02(args){
   return args.toUpperCase()
+}
+
+
+export function exercise03(args) {
+  const input = args
+  let result = 0
+
+  for (let i = 0; i < input.length; i++){
+    const currentElement = input[i]
+    if (currentElement === "e" || currentElement === "E"){
+      result++;
+    }
+  }
+
+  return result;
 }
