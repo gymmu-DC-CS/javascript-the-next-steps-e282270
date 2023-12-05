a5303cb0b7c5d5e57f34c8c82087c029810602c6
Caroline.Spaniol <carolinspaniol@gmail.com>
2023-12-05 15:49:32 +0100

    05.12.2023 Exercise 05

--- script.js
+++ script.js
@@ -35,3 +35,18 @@ export function exercise03(args) {
 
   return result;
 }
+
+export function exercise04(args) {
+  const input = args;
+  let result = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i];
+    // Check if the current character is a space and not followed by a dash
+    if (currentElement === " " && input[i + 1] !== "-") {
+      result++;
+    }
+  }
+
+  return result + 1;
+}
