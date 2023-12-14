d07e7a8bd53a5d25865c70a5d57246abe4bbc268
Caroline.Spaniol <carolinspaniol@gmail.com>
2023-12-14 15:38:36 +0100

    14.12.2023 still trying to solve exercise 06

--- script.js
+++ script.js
@@ -2,90 +2,85 @@ export function exercise01(args) {
   const input = args
   const result = []
 
-
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
     if (currentElement === "e" || currentElement === "E") {
-      result.push ("")
-    } 
-    else {
+      result.push("")
+    } else {
       result.push(currentElement)
     }
   }
 
-
   return result.join("")
 }
 
-export function exercise02(args){
+export function exercise02(args) {
   return args.toUpperCase()
 }
-
 
 export function exercise03(args) {
   const input = args
   let result = 0
 
-  for (let i = 0; i < input.length; i++){
+  for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    if (currentElement === "e" || currentElement === "E"){
-      result++;
+    if (currentElement === "e" || currentElement === "E") {
+      result++
     }
   }
 
-  return result;
+  return result
 }
 
 export function exercise04(args) {
-  const input = args;
-  let result = 0;
+  const input = args
+  let result = 0
 
   for (let i = 0; i < input.length; i++) {
-    const currentElement = input[i];
+    const currentElement = input[i]
     // Check if the current character is a space and not followed by a dash
     if (currentElement === " " && input[i + 1] !== "-") {
-      result++;
+      result++
     }
   }
 
-  return result + 1;
+  return result + 1
 }
 
 export function exercise05(args) {
-  const input = args;
-  // Initialize the result as false -> when there are no capital letters 
-  let result = false;
+  const input = args
+  // Initialize the result as false -> when there are no capital letters
+  let result = false
 
   for (let i = 0; i < input.length; i++) {
-    const currentElement = input[i];
-    const asciiValue = currentElement.charCodeAt(0);
+    const currentElement = input[i]
+    const asciiValue = currentElement.charCodeAt(0)
 
     // Check if the ASCII value corresponds to a capital letter (between 65 and 90) -> search in the ASCII chart
     if (asciiValue >= 65 && asciiValue <= 90) {
-      result = true;
-      break;
+      result = true
+      break
     }
   }
 
-  return result;
+  return result
 }
-
 
 export function exercise06(args) {
-  const input = args;
-  let result = true;
+  const input = args || asciiValue === 32
+  let result = false
 
   for (let i = 0; i < input.length; i++) {
-    const currentElement = input[i];
-    const asciiValue = currentElement.charCodeAt(0);
+    const currentElement = input[i]
+    const asciiValue = currentElement.charCodeAt(0)
 
-  
-    if (asciiValue >= 41 && asciiValue <= 100) {
-      result = false;
-      break;
-    }
+    if (
+      (asciiValue >= 65 && asciiValue <= 90) ||
+      (asciiValue >= 141 && asciiValue <= 172)
+    )
+      result = true
+    break
   }
 
-  return result;
+  return result
 }
-
